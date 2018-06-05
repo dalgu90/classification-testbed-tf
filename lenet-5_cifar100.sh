6e0b1dd8af0aa3421437e8a2914ba960c023cdf3
Juyong Kim <dalgu90@gmail.com>
2018-06-05 17:28:09 +0900

    add cifar-100 script

--- lenet-5_cifar100.sh
+++ lenet-5_cifar100.sh
@@ -6,12 +6,12 @@ python train.py --train_dir $train_dir \
     --network "lenet-5" \
     --dataset "cifar-100" \
     --data_dir "data/cifar-100-binary/" \
-    --num_classes 10 \
+    --num_classes 100 \
     --batch_size 500 \
     --test_interval 100 \
     --test_iter 20 \
     --l2_weight 0.0005 \
-    --initial_lr 0.1 \
+    --initial_lr 0.05 \
     --lr_step_epoch 100.0,200.0 \
     --lr_decay 0.1 \
     --max_steps 30000 \
